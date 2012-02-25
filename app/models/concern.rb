dd5fa7d389771f30eea930715dff3cfb7303cb70
Carl Brasic <carl.brasic@pearson.com>
2012-02-25 17:46:57 -0600

    method for quicker pageloads of users

--- app/models/concern.rb
+++ app/models/concern.rb
@@ -11,4 +11,16 @@ class Concern
 
   validates_presence_of :title
   validates_presence_of :content
+
+  # list of all the user ids used by the concerns and comments
+  def self.user_ids
+    all.map{|x| [x.updater_id.to_i] + x.comments.map{|y| y.updater_id.to_i rescue 0}}.flatten.uniq
+  end
+
+  # fetch the actual users that we care about for preloading onto the page
+  # this should be a full list of the users who have made comments or created concerns
+  def self.relevant_users
+    User.all_cached.select{|x| x.id.in? self.user_ids}
+  end
+
 end
